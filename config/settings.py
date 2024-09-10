26f69f39710faaf5b23424dfbfa7a31c5e434498
Michael Andrew Chan <michaelac978@gmail.com>
2024-09-10 14:59:37 +0800

    chore: add cors headers

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
 
     'apps.courses',
@@ -60,6 +61,25 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOW_HEADERS = [
+    "Content-Type", "Authorization"
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://backend.ntusu.org',
+]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'https://backend.ntusu.org',
+) 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'https://backend.ntusu.org',
+]
 
 TEMPLATES = [
     {
