33dd00b5156d68c00614ed13578b1906ad0b0378
Michael Andrew Chan <michaelac978@gmail.com>
2024-09-11 16:13:20 +0800

    feat: optimizer api template, delete unused files

--- apps/optimizer/views.py
+++ apps/optimizer/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.optimizer.serializers import OptimizerInputSerialzer
+from apps.optimizer.algo import optimize_index
+
+
+class OptimizeView(generics.CreateAPIView):
+    serializer_class = OptimizerInputSerialzer
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        output = optimize_index(serializer.validated_data)
+        return Response(output)
