1e6285abd610327ff3602385bcbee00e93f93676
Michael Andrew Chan <michaelac978@gmail.com>
2024-09-12 15:30:32 +0800

    feat: add FeedbackForm model and migration

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.feedback.models import FeedbackForm
+
+
+admin.site.register(FeedbackForm)
--- apps/feedback/models.py
+++ apps/feedback/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone as tz
 
-# Create your models here.
+
+class FeedbackForm(models.Model):
+    '''
+    FeedbackForm model to store feedbacks from users. Fields include:
+    - contact: optional contact information (email / telegram / etc.) for follow-up
+    - type: type of feedback (bug report, feature request, etc.)
+    - title: title of the feedback
+    - details: details of the feedback
+    '''
+    class Type(models.TextChoices):
+        BUG_REPORT = 'Bug Report', 'Bug Report'
+        FEATURE_REQUEST = 'Feature Request', 'Feature Request'
+        IMPROVEMENT_SUGGESTION = 'Improvement Suggestion', 'Improvement Suggestion'
+        REQUEST_ASSISTANCE = 'Request Assistance', 'Request Assistance'
+        OTHERS = 'Others', 'Others'
+
+    contact = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=30, choices=Type.choices)
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    creation_time = models.DateTimeField(default=tz.now)
+
+    '''
+    Fields for internal use only (modify through admin page):
+    - acknowledged: changes to True when feedback is acknowledged
+    - resolved: changes to True when feedback is resolved
+    - internal_notes: any notes for internal use
+    '''
+    acknowledged = models.BooleanField(default=False)
+    resolved = models.BooleanField(default=False)
+    internal_notes = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Feedback Forms'
+
+    def __str__(self):
+        return f'<Feedback #{self.id}: ({self.type}) {self.title}>'
