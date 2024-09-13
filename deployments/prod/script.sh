d8a7624a94b6ff50a791185c08e521ae744d3179
Michael Andrew Chan <michaelac978@gmail.com>
2024-09-13 12:32:06 +0800

    build: setup whitenoise, static files for prod env

--- deployments/prod/script.sh
+++ deployments/prod/script.sh
@@ -1,2 +1,2 @@
-docker build -f deployments/prod/Dockerfile -t ntumods_prod_backend .
-docker run -d -p 8000:8000 --env-file .env ntumods_prod_backend
+docker build -f deployments/prod/Dockerfile -t ntumods_api_prod .
+docker run -d -p 8000:8000 --env-file .env --name ntumods_api_prod ntumods_api_prod
